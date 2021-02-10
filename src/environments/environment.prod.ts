fe6a081103af64b804a6946b429ac0fe635ef14a
guystephane <guystephane00@gmail.com>
2021-02-10 00:34:16 +0000

    fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  base_url: 'https://api.souko.me',
+  base_url: 'https://api.souko.me/api/v1/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // base_url: 'http://localhost:8000/api/v1/',
-  base_url: 'https://api.souko.me',
+  base_url: 'https://api.souko.me/api/v1/',
 };
 
 /*
