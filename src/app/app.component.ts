f82044f6417edb2609aedca4457b2ee81dff3768
guystephane <guystephane00@gmail.com>
2021-02-12 01:47:49 +0000

    fix header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { TimelineMax, Back } from 'gsap';
 
 declare let gtag: Function;
 
@@ -10,7 +9,6 @@ declare let gtag: Function;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  menu = new TimelineMax( { ease: Back.easeOut.config(2), paused: true } );
 
   constructor(public router: Router){
     this.router.events.subscribe( event => {
@@ -24,16 +22,5 @@ export class AppComponent implements OnInit {
     } );
   }
 
-  ngOnInit(): void {
-    this.createMenuAnim();
-  }
-
-  createMenuAnim(){
-    this.menu.to( '.menu-container', .2, { opacity: 0, y: '-30px', stagger: 0.1 } );
-    this.menu.to( '.menu-wrapper', .2, { clipPath: 'circle(0%)' }, '-=.2' );
-  }
-
-  toggleMenu() {
-    this.menu.restart();
-  }
+  ngOnInit(): void { }
 }
