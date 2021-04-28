312e6d8b154842a8469c616bb1c92f5b993442ba
guystephane <guystephane00@gmail.com>
2021-04-28 16:29:11 +0000

    fix landing page

---
+++ src/app/pages/landing/countdown/countdown.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.css']
+})
+export class CountdownComponent implements OnInit {
+  @Input() config;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
